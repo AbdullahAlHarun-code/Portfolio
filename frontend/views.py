e47e4ec8e58fa17ccd979858ac5f41673fad0697
abdullahcodeacademy <abdullahcodeacademy@gmail.com>
2023-01-09 01:15:48 +0000

    add some new feature in views and template

--- frontend/views.py
+++ frontend/views.py
@@ -20,8 +20,11 @@ def home(request):
     # sub_category_blocks = SubCategory.objects.filter(blocks__in=[2])
     # for block in sub_category_blocks:
     #     print('Block',block)
+    all_blocks = Block.objects.filter(active=True).order_by('hierarchy')
     blocks = Block.objects.filter(active=True, sub_category__parent_category__name='services').order_by('hierarchy')
     services_card = blocks.filter(sub_category__name='service section card')
+    resume_education = all_blocks.filter(sub_category__parent_category__name='resume', sub_category__name='education')
+    resume_experience = all_blocks.filter(sub_category__parent_category__name='resume', sub_category__name='experience')
     personal_info = PersonalInfo()
     section_one = SectionOne()
     default_info =DefaultInfo()
@@ -34,6 +37,8 @@ def home(request):
         'section_one': SectionOne,
         'default_info': default_info,
         'about': about,
-        'services':services
+        'services':services,
+        'resume_education': resume_education,
+        'resume_experience': resume_experience,
         }
     return render(request, template_name, context)
